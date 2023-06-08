fb784d3c364bcc4cd6044fe24b8b1ee00891c4a3
akbar hossen <mdakbar758742@gmail.com>
2023-06-08 11:38:33 -0700

    update

--- c++/array operation.cpp
+++ c++/array operation.cpp
@@ -1,1 +1,32 @@
 
+#include <stdio.h>
+#include <string.h>
+
+void insertCharacter(char array[], int position, char character) {
+    int length = strlen(array);
+
+    if (position < 0 || position > length) {
+        printf("Invalid position\n");
+        return;
+    }
+
+    for (int i = length; i >= position; i--) {
+        array[i + 1] = array[i];
+    }
+
+    array[position] = character;
+}
+
+int main() {
+    char array[50] = "Hello, world!";
+    int position = 5;
+    char character = '!';
+
+    printf("Original array: %s\n", array);
+
+    insertCharacter(array, position, character);
+
+    printf("Array after insertion: %s\n", array);
+
+    return 0;
+}
