180b9c3e0371d0235356a294e2a00f86f87e29fe
akbar hossen <mdakbar758742@gmail.com>
2023-06-10 20:47:41 -0700

    update

---
+++ codeforces/bit++.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,X=0;
+      string s;
+    cin>>n;
+
+    while(n--)
+    {
+
+        cin>>s;
+        if(s=="++X" || s=="X++")
+        {
+            X++;
+        }
+        else
+            X--;
+    }
+    cout<<X<<endl;
+
+return 0;
+}
+
