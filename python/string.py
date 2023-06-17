5d2837de27699138f8dab4694c6932f85619842f
akbar hossen <mdakbar758742@gmail.com>
2023-06-17 10:14:51 -0700

    update

--- python/string.py
+++ python/string.py
@@ -2,11 +2,16 @@ course='hi brob'
 another='''
 hi akbar,
 this is to inform you for your refreshment
-the support team
+the support team dfdf df
 '''
 print(course[1:-1])
 print(another)
 mess='md'
 msg='akbar'
+i='hey mishal abir tanim bro'
+h='how are you'
+tex=f'{i}{h}?'
 mix=f' {mess} {msg} is a coder'
 print(mix)
+print(tex)
+print(len(another))
