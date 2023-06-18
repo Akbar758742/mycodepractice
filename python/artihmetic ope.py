a3727c313fe9bbe5cc91f003917cd8f463edb625
akbar hossen <mdakbar758742@gmail.com>
2023-06-17 23:58:45 -0700

    update

---
+++ python/artihmetic ope.py
@@ -0,0 +1,7 @@
+print(2**3)#power
+print(10//3)#inte
+print(10/3)
+print(10%3)#mod
+x=5
+x*=5
+print(x)
---
+++ python/math funtion.py
@@ -0,0 +1,6 @@
+x=3.5
+print(round(x))
+print(abs(-3.4))
+import  math
+print(math.floor(x))
+print(math.ceil(x))
