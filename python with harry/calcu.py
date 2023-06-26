8f0f25ee81407b5f6493677e274c9177ed9b2932
akbar hossen <mdakbar758742@gmail.com>
2023-06-26 12:23:26 -0700

    update

--- python with harry/calcu.py
+++ python with harry/calcu.py
@@ -4,5 +4,5 @@ add=a+b
 print("The addition of",a,"+",b,"is:",add)
 print("The subtraction of",a,"-",b,"is:",a-b)
 print("The multiplication of",a,"*",b,"is:",a*b)
-print("The division of",a,"/",b,"is:",a/b)
+print("The division of",a,"/",b,"is:",a/b)  
 print("The modulus of",a,"%",b,"is:",a%b)
